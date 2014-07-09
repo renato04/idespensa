687d989b952e972936262c6a06e881e42085920f
Renato Ramos Nascimento <ramos.renato@outlook.com>
2014-07-09 13:54:47 -0300

    modificado a lista de produtos

--- js/controllers.js
+++ js/controllers.js
@@ -38,23 +38,72 @@ angular.module('ionicApp.controllers', ['ionicApp.config', 'xc.indexedDB'])
     // Update scope
     $scope.safeApply(function(){
 
+       alert(JSON.stringify(results));
+       alert(JSON.stringify(results[0]));
         //$scope.items = results;
-        $scope.items = _.groupBy(results, function(item) {return item.nome[0]; });    
+        $scope.items = results;    
 
     });
   }); 
    
 })
-.controller('ProdutoCadastroController',function($scope, $ionicPopup, $timeout, $ionicModal, $window){
+.controller('ProdutoCadastroController',function($scope, $indexedDB, $ionicNavBarDelegate, $ionicPopup){
 
-  var OBJECT_STORE_NAME = constants.ProdutoStore; 
-  $scope.produto = {};
-
-  $scope.save = function(produto){
-
-    alert(JSON.stringify(produto));
+  $scope.safeApply = function(fn) {
+    var phase = this.$root.$$phase;
+    if(phase == '$apply' || phase == '$digest') {
+      if(fn && (typeof(fn) === 'function')) {
+        fn();
+      }
+    } else {
+      this.$apply(fn);
+    }
   };
 
+  var PRODUTO_STORE_NAME = constants.ProdutoStore; 
+  var CATEGORIA_STORE_NAME = constants.CategoriaStore; 
+  
+  $scope.produto = {};
+  $scope.categorias = [];
+
+  var categoriaObjectStore = $indexedDB.objectStore(CATEGORIA_STORE_NAME);
+
+  categoriaObjectStore.getAll().then(function(results) {  
+    // Update scope
+    $scope.safeApply(function(){
+
+        //$scope.items = results;
+        $scope.categorias = results;    
+
+    });
+  });   
+
+  $scope.save = function(produto){
+    
+    var myObjectStore = $indexedDB.objectStore(PRODUTO_STORE_NAME);
+
+    myObjectStore.insert({"id": Guid.raw(),
+                          "nome": produto.nome,
+                          "quantidade": produto.quantidade,
+                          "categoria": produto.categoria,
+                          "data_validade": produto.data_validade,
+                          "quantidade_lista": produto.quantidade_lista}
+                        )
+                        .then(
+                              function(){
+
+                                 $ionicPopup.alert({
+                                   title: 'iDespensa',
+                                   template: 'Produto cadastrado com sucesso.'
+                                 }).then( function(){
+                                      $scope.safeApply(function(){
+                                        $ionicNavBarDelegate.back();
+                                      });
+                                 });    
+
+
+                        });
+  };
 })
 .controller('CategoriaController',function($scope, $indexedDB, $ionicNavBarDelegate, $ionicPopup){
 
@@ -81,13 +130,6 @@ angular.module('ionicApp.controllers', ['ionicApp.config', 'xc.indexedDB'])
       // Update scope
       $scope.safeApply(function(){
 
-          if (results.length == 0) {
-             var alertPopup = $ionicPopup.alert({
-               title: 'iDespensa',
-               template: 'Nâo há categoria cadastradas.'
-             });
-          }
-
           //$scope.items = results;
           $scope.items = _.groupBy(results, function(item) {return item.nome[0]; });    
 
@@ -106,10 +148,19 @@ angular.module('ionicApp.controllers', ['ionicApp.config', 'xc.indexedDB'])
         var myObjectStore = $indexedDB.objectStore(OBJECT_STORE_NAME);
 
         myObjectStore.insert({"id": Guid.raw(), "nome": categoria.nome}).then(
-              function(){
-                  $ionicNavBarDelegate.back();
-              }  
-          );
+                              function(){
+
+                                 $ionicPopup.alert({
+                                   title: 'iDespensa',
+                                   template: 'Categoria cadastrada com sucesso.'
+                                 }).then( function(){
+                                      $scope.safeApply(function(){
+                                          $ionicNavBarDelegate.back();
+                                      });
+                                 });    
+
+
+                        });
 
     };       
 });
