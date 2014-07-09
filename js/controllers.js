e50ca5f9a15d65aa593cd77bc2c20412ebaa29fa
Renato Ramos Nascimento <ramos.renato@outlook.com>
2014-07-09 11:55:18 -0300

    adicionado o formulario de produtos

--- js/controllers.js
+++ js/controllers.js
@@ -14,19 +14,47 @@ angular.module('ionicApp.controllers', ['ionicApp.config', 'xc.indexedDB'])
        
 
 })
-.controller('ProdutoController',function($scope, $ionicPopup, $timeout, $ionicModal, $window){
+.controller('ProdutoController',function($scope, $ionicPopup, $timeout, $ionicModal, $indexedDB, $window){
+
+  $scope.safeApply = function(fn) {
+    var phase = this.$root.$$phase;
+    if(phase == '$apply' || phase == '$digest') {
+      if(fn && (typeof(fn) === 'function')) {
+        fn();
+      }
+    } else {
+      this.$apply(fn);
+    }
+  };
 
   var OBJECT_STORE_NAME = constants.ProdutoStore; 
+  $scope.items = [];
+  $scope.produto = {}; 
+  $scope.predicate = "nome";
 
-     
+  var myObjectStore = $indexedDB.objectStore(OBJECT_STORE_NAME);
+
+  myObjectStore.getAll().then(function(results) {  
+    // Update scope
+    $scope.safeApply(function(){
+
+        //$scope.items = results;
+        $scope.items = _.groupBy(results, function(item) {return item.nome[0]; });    
+
+    });
+  }); 
+   
 })
 .controller('ProdutoCadastroController',function($scope, $ionicPopup, $timeout, $ionicModal, $window){
 
   var OBJECT_STORE_NAME = constants.ProdutoStore; 
-  $scope.save = function(novoProduto){
+  $scope.produto = {};
 
+  $scope.save = function(produto){
 
+    alert(JSON.stringify(produto));
   };
+
 })
 .controller('CategoriaController',function($scope, $indexedDB, $ionicNavBarDelegate, $ionicPopup){
 
